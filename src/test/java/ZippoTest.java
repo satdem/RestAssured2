b97177926660ed6cd01ce0f76cf694ebd7e76b4b
ismettechno <ismettechno@gmail.com>
2020-11-30 18:58:55 +0000

    given.when.then

--- src/test/java/ZippoTest.java
+++ src/test/java/ZippoTest.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class ZippoTest {
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
+public class ZippoTest {
+
+    @Test
+    public void test()
+    {
+        given()
+                .when()
+                .then();
+    }
+
 }
