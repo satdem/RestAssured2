5daeed409cb224461353c70dc75d2aaef7659e6f
ismettechno <ismettechno@gmail.com>
2020-12-01 18:27:01 +0000

    statusCodeTest

--- src/test/java/ZippoTest.java
+++ src/test/java/ZippoTest.java
@@ -8,9 +8,23 @@ public class ZippoTest {
     @Test
     public void test()
     {
+        given() // balagıç ayarları (setup) işlemleri
+                .when()  // aksiyon kısmı
+                .then(); // test kısmı
+    }
+
+    @Test
+    public void statusCodeTest()
+    {
         given()
+
                 .when()
-                .then();
+                .get("http://api.zippopotam.us/us/90210")
+
+                .then()
+                .log().all()
+                .statusCode(200)
+        ;
     }
 
 }
