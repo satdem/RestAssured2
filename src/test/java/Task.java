7622f426eb1bae8316adb0b8ca7b00e4b68a44a8
ismettechno <ismettechno@gmail.com>
2020-12-07 18:33:41 +0000

    Task 2 Question

--- src/test/java/Task.java
+++ src/test/java/Task.java
@@ -8,6 +8,13 @@ public class Task {
      * expect content type TEXT
      */
 
+    /** Task 2
+     * create a request to https://httpstat.us/418
+     * expect status 418
+     * expect content type TEXT
+     * expect body to be equel to "418 I'm a teapot"
+     */
+
 
 
 
