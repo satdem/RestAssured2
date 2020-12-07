13dee74808386a402680098d4718d34037a5ba76
ismettechno <ismettechno@gmail.com>
2020-12-07 19:34:07 +0000

    Task 6,7,8 Questions

--- src/test/java/Task.java
+++ src/test/java/Task.java
@@ -38,6 +38,21 @@ public class Task {
      *      userId = 1
      */
 
+    /** Task 6
+     * create a request to https://jsonplaceholder.typicode.com/todos/2
+     * expect status 200
+     * Converting Into POJO
+     */
 
+    /** Task 7
+     * create a request to https://jsonplaceholder.typicode.com/todos
+     * expect status 200
+     * Converting Array Into Array of POJOs
+     */
 
+    /** Task 8
+     * create a request to https://jsonplaceholder.typicode.com/todos
+     * expect status 200
+     * Converting Array Into List of POJOs
+     */
 }
