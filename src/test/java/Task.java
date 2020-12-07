73b1833c07b11935d70a327659b4f16144dbe85b
ismettechno <ismettechno@gmail.com>
2020-12-07 18:30:41 +0000

    Task 1 Solution

--- src/test/java/Task.java
+++ src/test/java/Task.java
@@ -1,2 +1,15 @@
+
+
 public class Task {
+
+    /** Task 1
+     * create a request to https://httpstat.us/203
+     * expect status 203
+     * expect content type TEXT
+     */
+
+
+
+
+
 }
--- src/test/java/TaskSolution.java
+++ src/test/java/TaskSolution.java
@@ -1,10 +1,23 @@
+import io.restassured.http.ContentType;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+
 public class TaskSolution {
 
-    /** Task 1
-     * create a request to https://httpstat.us/203
-     * expect status 203
-     * expect content type TEXT
-     */
+    @Test
+    public void task1()
+    {
+        given()
+                .when()
+                .get("https://httpstat.us/203")
+                .then()
+                .log().body()
+                .statusCode(203)
+                .contentType(ContentType.TEXT)
+        ;
+    }
 
 
 
