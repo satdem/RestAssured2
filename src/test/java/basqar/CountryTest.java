e4e870c174db7f68e5c94668d61ea1b78ae1779a
ismettechno <ismettechno@gmail.com>
2020-12-21 19:10:19 +0000

    hatalı duurmlar için mail alabilmek için deleteById de statusCode kontrolü düzeltildi

--- src/test/java/basqar/CountryTest.java
+++ src/test/java/basqar/CountryTest.java
@@ -131,7 +131,7 @@ public class CountryTest {
                 .when()
                 .delete("/school-service/api/countries/{countryId}")
                 .then()
-                .statusCode(201)
+                .statusCode(200)
                 .body(equalTo(""))
                 ;
     }
